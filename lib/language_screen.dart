367c446a920acdd9a6adc136037e749a23cc1f9e
Safaa55555 <140979793+Safaa55555@users.noreply.github.com>
2025-07-21 23:47:32 +0300

    lang and splash screens

---
+++ lib/language_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+class LanguageScreen extends StatelessWidget {
+  const LanguageScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+      Text("Choose Your Language"),
+      ElevatedButton(onPressed: (){}, child: Text("English")),
+      ElevatedButton(onPressed: (){}, child: Text("Arabic"))
+    ],),);
+  }
+}
+
