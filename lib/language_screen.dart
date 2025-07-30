b64baccc38ddfda840bd27ba4fb4c7fd3109d47e
HudaOmer <hudaoe175@gmail.com>
2025-07-30 16:29:02 +0200

    Add Const

--- lib/language_screen.dart
+++ lib/language_screen.dart
@@ -1,16 +1,20 @@
 import 'package:flutter/material.dart';
+
 class LanguageScreen extends StatelessWidget {
   const LanguageScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: [
-        Text("Choose Your Language"),
-        ElevatedButton(onPressed: (){}, child: Text("English")),
-        ElevatedButton(onPressed: (){}, child: Text("Arabic"))
-      ],),);
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          const Text("Choose Your Language"),
+          ElevatedButton(onPressed: () {}, child: const Text("English")),
+          ElevatedButton(onPressed: () {}, child: const Text("Arabic"))
+        ],
+      ),
+    );
   }
 }
